b304025f90f56eb7f27c2101e44b29a66b1743aa
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2024-11-24 14:37:56 +0000

    push

---
+++ first-app/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Header"
+import Footer from './Footer'
+function App(){
+    return(
+        <>
+         <Header />
+         <h1>Hello World</h1>
+         <Footer />
+        </>
+    )
+}
+export default App
---
+++ first-app/src/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer (){
+    return (
+        <footer>&copy Ourouimed 2024</footer>
+    )
+}
